2b8f95dcb462a9c3cb9924e649bfe4a6ce160d4a
mud pirate <mudpirate3@gmail.com>
2025-06-20 18:28:52 +0530

    web socket tutorial

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static("./client"));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
